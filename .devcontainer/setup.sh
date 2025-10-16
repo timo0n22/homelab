151f3791aa72e81d8de083542676c980e0137e75
loginovartem <akkdlyaandroid@gmail.com>
2025-10-16 16:07:41 +0600

    fix kuberconfig and devcontainer mounts

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,7 +16,7 @@ nix-env -iA nixpkgs.fzf \
              nixpkgs.yaml-language-server \
              nixpkgs.marksman
 
-git clone --depth 1 https://github.com/timo0n22/devcontainers-dotfiles.git /tmp/dotfiles
+git clone --depth 1 git@github.com:timo0n22/devcontainers-dotfiles.git /tmp/dotfiles
 cp -r /tmp/dotfiles ~/.dotfiles
 rm -rf /tmp/dotfiles
 
@@ -44,3 +44,37 @@ k9s:
     skin: dark
     logoless: false
 EOF
+
+echo "Настройка kubeconfig..."
+
+cat > ~/.kube/config << 'KUBEEOF'
+apiVersion: v1
+kind: Config
+clusters:
+- cluster:
+    server: https://80.76.34.151:6443
+    insecure-skip-tls-verify: true
+  name: homelab
+contexts:
+- context:
+    cluster: homelab
+    user: homelab
+  name: homelab
+current-context: homelab
+users:
+- name: homelab
+  user:
+    exec:
+      apiVersion: client.authentication.k8s.io/v1beta1
+      command: ssh
+      args:
+        - control
+        - sudo
+        - k3s
+        - kubectl
+        - config
+        - view
+        - --raw
+KUBEEOF
+
+chmod 600 ~/.kube/config
